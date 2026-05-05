e2776efa29830c5b363cb29d789d0017c9eade78
prince Aime <princerich84@gmail.com>
2026-05-05 23:43:52 +0200

    Force add server config file

---
+++ src/server.js
@@ -0,0 +1,3 @@
+export const server = "https://guriraline-server-fixes.onrender.com/api/v2";
+export const backend_url = "https://guriraline-server-fixes.onrender.com/";
+export const socketEndpoint = "https://guriraline-server-fixes.onrender.com";
